4083ed5c13f6d4613788f55bf8b5b42907004c47
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-13 04:03:26 -0300

    2. Agora vamos fazer com que as moedas apareçam na tela.

--- apiCoins.js
+++ apiCoins.js
@@ -2,7 +2,14 @@ const fetchCoins = async () => {
     const URL = 'https://api.coincap.io/v2/assets';
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(data.data);
-};
+    const result = data.data;
+    result.map((asset) => {
+        const {name, symbol, priceUsd} = asset;
+        const elementUl = document.getElementById("coins-list");
+        const elementLi = document.createElement("li");
+        elementUl.appendChild(elementLi);
+        elementLi.innerText = `${asset.name} (${symbol}): ${Number(priceUsd).toFixed(2)}`;
+    })
 
+};
 fetchCoins();
