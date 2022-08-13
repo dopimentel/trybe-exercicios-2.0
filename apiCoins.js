1d27e15688dc4a1ee0729e8211071f2d6674e61a
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-13 02:53:20 -0300

    Requisito  - 1 . Vamos consultar uma API que fornece os valores de crypto moedas

--- apiCoins.js
+++ apiCoins.js
@@ -1,4 +1,8 @@
 const fetchCoins = async () => {
+    const URL = 'https://api.coincap.io/v2/assets';
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data.data);
 };
 
 fetchCoins();
