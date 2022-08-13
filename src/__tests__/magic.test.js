ca4915fc592d25c1aa5a4b57b16e388ef56b85f0
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-13 20:19:54 -0300

    assertions

--- src/__tests__/magic.test.js
+++ src/__tests__/magic.test.js
@@ -5,15 +5,18 @@ jest.setTimeout(10000);
 
 describe('1. Testes da função getMagicCard', () => {
   it('1.1 Verifique se getMagicCard é uma função.', () => {
+    expect.assertions(1);
     expect(typeof getMagicCard).toBe('function');
   });
 
   it('1.2 Verifique se a função fetch foi chamada.', async () => {
+    expect.assertions(1);
     await getMagicCard('130550');
     expect(fetch).toHaveBeenCalledTimes(1);
   });
 
   it('1.3 Verifique se a função fetch foi chamada com o endpoint correto', async () => {
+    expect.assertions(1);
     await getMagicCard('130550');
     const URL = 'https://api.magicthegathering.io/v1/cards/130550';
     expect(fetch).toHaveBeenCalledWith(URL);
@@ -22,6 +25,7 @@ describe('1. Testes da função getMagicCard', () => {
 
 describe('2. Testando o retorno da função getMagicCard', () => {
   it('2.1 Verifique se a carta retornada possui o nome Ancestor\'s Chosen.', async () => {
+    expect.assertions(1);
     const response = await getMagicCard('130550');
     const { name } = response;
     expect(name).toBe('Ancestor\'s Chosen');
